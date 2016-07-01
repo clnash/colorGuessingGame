bec6b7c4969a3317f56ddefe761a5099f004de25
Christopher Nash <nash_web@outlook.com>
2016-07-01 18:41:59 +0000

    Made more progress

---
+++ app.js
@@ -0,0 +1,24 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255,255, 0)"
+    ]
+
+var squares = document.querySelectorAll('.square');
+
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById('#colorDisplay');
+
+
+
+
+
+for (var i = 0; 1 < squares.length; i++){
+    
+    squares[i].style.background = colors[i];
+    
+}
